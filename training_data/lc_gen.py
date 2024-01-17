a97038229e2b929d158f69fc4c04ed3ce17c8ebe
robbie-webbe <robbie.webbe@bristol.ac.uk>
2024-01-17 11:41:34 +0100

    Initial commit to set up structure.

---
+++ training_data/lc_gen.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 17 09:58:02 2024
+
+@author: rwebbe
+"""
+
+import sys
+import os
+sys.path.append(os.getcwd()[:-20]+'Analysis_Funcs/LC_Sim/')
+
+from TKLC import TK_LC
